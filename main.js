cc91e5e7f5935f948769070577daf6a685b6cd83
Pimlada Kongkham <pimdiamond@gmail.com>
2021-06-13 16:30:39 -0400

    added js file

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onmouseover  = myFunction;
+
+function myFunction() {
+  document.getElementsByClassName("b1")[0].style.backgroundColor = "green";
+    document.getElementsByClassName("b2")[0].style.backgroundColor = "white";
+      document.getElementsByClassName("b3")[0].style.backgroundColor = "red";
+}
